5e4e78036b64617f8a8cc3e68bf1ada5e5c529bd
Jan Weil <jan.weil@ptb.de>
2015-02-12 11:23:17 +0100

    Add types.h

---
+++ mbb/types.h
@@ -0,0 +1,12 @@
+#ifndef MBB_TYPES_H
+#define MBB_TYPES_H
+
+#ifdef MBB_SYSTEM_TYPES_HEADER
+# include MBB_SYSTEM_TYPES_HEADER
+# else
+# include <stdint.h>
+# include <stdbool.h>
+# include <stddef.h>
+#endif
+
+#endif /* MBB_TYPES_H */
