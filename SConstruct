3e6c4bce36bc073e0e691d496135347dfd774806
Jan Weil <jan.weil@web.de>
2015-02-16 13:23:15 +0100

    Add unit test module

--- SConstruct
+++ SConstruct
@@ -1,3 +1,5 @@
+import os.path
+
 env = Environment(CPPPATH='.', LIBPATH='mbb')
 env.Library('mbb/mbb', Glob('mbb/*.c'))
 
@@ -10,3 +12,21 @@ if not conf.CheckHeader('termios.h'):
 
 for ex in examples:
 	env.Program(ex, LIBS=['mbb'])
+env['BUILDERS']['TestBuilder'] = Builder(action = 'tools/munt_main $SOURCE > $TARGET')
+
+tests = Glob('tests/*.c', True, False, True)
+
+test_aliases = []
+
+for test in tests:
+	test_main = "tests/build/" + os.path.basename(test.replace(".c", "_main.c"))
+	test_out = test.replace(".c", "")
+	env.TestBuilder(test_main, test)
+	p = env.Program(test_out, [test_main], LIBS=['mbb'])
+	a = Alias(test, [p], p[0].path)
+	AlwaysBuild(a)
+	test_aliases.append(a)
+
+env.Clean(tests, "tests/build")
+
+env.Alias('test', test_aliases)
---
+++ mbb/test.h
@@ -0,0 +1,56 @@
+#ifndef MBB_TEST_H
+#define MBB_TEST_H
+
+#include <stdio.h>
+
+#if __linux
+/*
+ * vt100-compatible text colors
+ * http://linuxgazette.net/issue65/padala.html
+ */
+void textcolor(int attr, int fg, int bg);
+
+#define MUNT_RESET	0
+#define MUNT_BRIGHT	1
+#define MUNT_BLACK	0
+#define MUNT_RED	1
+#define MUNT_WHITE	7
+
+#define MUNT_COLOR(statement) do { \
+	textcolor(MUNT_BRIGHT, MUNT_RED, MUNT_BLACK); \
+	statement; \
+	textcolor(MUNT_RESET, MUNT_WHITE, MUNT_BLACK); \
+} while (0) 
+#else
+#define MUNT_COLOR(statement) do { \
+	statement; \
+} while (0)
+#endif /* __linux */
+
+/* 
+ * Minunit-like test 'framework'
+ * http://www.jera.com/techinfo/jtns/jtn002.html
+ */
+#define MUNT_ASSERT(TEST) do { \
+	static char msgstr[256]; \
+	snprintf(msgstr, sizeof(msgstr), "Assertion FAILED (%03d): %s", __LINE__, #TEST); \
+	if (!(TEST)) return msgstr; \
+	fprintf(stderr, "Assertion passed (%03d): %s\n", __LINE__, #TEST); \
+} while (0)
+
+int munt_tests_run;
+
+#define MUNT_RUN_TEST(test) do { \
+	char *message; \
+	fprintf(stderr, "### " #test " ###\n"); \
+	message = test(); \
+	munt_tests_run++; \
+	if (message) { \
+		MUNT_COLOR(fprintf(stderr, "### " #test " FAILED: '%s' ###\n\n", message)); \
+		return message; \
+	} else { \
+		fprintf(stderr, "### passed ###\n\n"); \
+	} \
+} while (0)
+
+#endif /* MBB_TEST_H */
