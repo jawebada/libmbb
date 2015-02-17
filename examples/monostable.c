f9b49019009a99293c95511d6b9b19a6bd308c44
Jan Weil <jan.weil@web.de>
2015-02-17 17:03:51 +0100

    Convert monostable example to libev timers

--- examples/monostable.c
+++ examples/monostable.c
@@ -19,10 +19,11 @@
  */
 
 #include "mbb/hsm.h"
-#include "mbb/timer.h"
+#include "mbb/timer_ev.h"
 #include "mbb/debug.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ev.h>
 
 #define MONO_NROF_SWITCHES	5
 #define MONO_TIMEOUT		(2 * MTMR_ONE_SEC)
@@ -35,28 +36,14 @@ enum {
 };
 
 typedef struct {
-	/* the state structure must start with an array of mtmr_t */
-	mtmr_t timers[MTMR_NROF_TIMERS(MONO_EVENT_TIMEOUT)];
+	/* the state structure must start with an array of timers */
+	mtmr_ev_t timer[MTMR_NROF_TIMERS(MONO_EVENT_TRIGGER)];
 	int id;
 	int counter;
 } mono_state_t;
 
-MHSM_DEFINE_STATE(mono_top, NULL);
-MHSM_DEFINE_STATE(mono_off, &mono_top);
-MHSM_DEFINE_STATE(mono_on, &mono_top);
-
-mhsm_state_t *mono_top_fun(mhsm_hsm_t *hsm, mhsm_event_t event)
-{
-	mono_state_t *state = (mono_state_t*) mhsm_context(hsm);
-
-	switch (event.id) {
-		case MHSM_EVENT_DO:
-			printf(" %02d (%03d)\t", state->id, state->counter);
-			break;
-	}
-
-	return &mono_top;
-}
+MHSM_DEFINE_STATE(mono_off, NULL);
+MHSM_DEFINE_STATE(mono_on, NULL);
 
 mhsm_state_t *mono_off_fun(mhsm_hsm_t* hsm, mhsm_event_t event)
 {
@@ -64,13 +51,10 @@ mhsm_state_t *mono_off_fun(mhsm_hsm_t* hsm, mhsm_event_t event)
 
 	switch (event.id) {
 		case MHSM_EVENT_ENTRY:
-			MDBG_PRINT1("%d off\n", state->id);
+			MDBG_PRINT2("OFF %02d (%03d)\n", state->id, state->counter);
 			break;
 		case MONO_EVENT_TRIGGER:
 			return &mono_on;
-		case MHSM_EVENT_DO:
-			printf("OFF");
-			break;
 	}
 
 	return &mono_off;
@@ -82,78 +66,86 @@ mhsm_state_t *mono_on_fun(mhsm_hsm_t *hsm, mhsm_event_t event)
 
 	switch (event.id) {
 		case MHSM_EVENT_ENTRY:
-			MDBG_PRINT1("%d on\n", state->id);
 			state->counter++;
 			/* fall-through */
 		case MONO_EVENT_TRIGGER:
+			MDBG_PRINT2("ON  %02d (%03d)\n", state->id, state->counter);
 			mhsm_start_timer(hsm, MONO_EVENT_TIMEOUT, MONO_TIMEOUT);
 			break;
 		case MONO_EVENT_TIMEOUT:
 			return &mono_off;
-		case MHSM_EVENT_DO:
-			printf("ON ");
-			break;
 	}
 
 	return &mono_on;
 }
 
-#include "periodic.inc"
-#include "keyboard.inc"
-
-int process(mhsm_hsm_t *switches, void *state)
+static void print_cb(EV_P_ ev_timer *w, int revents)
 {
+	mhsm_hsm_t *switches = (mhsm_hsm_t*) w->data;
 	int i;
 
-	if (kbhit()) {
-		char c = fgetc(stdin);
-
-		MDBG_PRINT_C(c);
-
-		if (c >= '1' && c < ('1' + MONO_NROF_SWITCHES)) 
-			mhsm_dispatch_event(switches + (c - '1'), MONO_EVENT_TRIGGER);
-
-		if (c == 'q') return -1;
-	}
-
 	printf("\r");
-
 	for (i = 0; i < MONO_NROF_SWITCHES; i++) {
-		mtmr_increment_timers(switches + i, MONO_EVENT_TIMEOUT, MONO_PERIOD);
-		mhsm_dispatch_event(switches + i, MHSM_EVENT_DO);
+		mhsm_hsm_t *hsm = switches + i;
+		mono_state_t *state = (mono_state_t*) mhsm_context(hsm);
+
+		printf("%d: %s (%03d)\t", state->id, mhsm_is_in(hsm, &mono_on) ? "ON " : "OFF", state->counter);
+	}
+	fflush(stdout);
+		
+}
+
+static void keypress_cb(EV_P_ ev_io *w, int revents)
+{
+	mhsm_hsm_t *switches = (mhsm_hsm_t*) w->data;
+	char c;
+
+	c = getchar();
+
+	if (c >= '1' && c < '1' + MONO_NROF_SWITCHES) {
+		mhsm_dispatch_event(switches + (c - '1'), MONO_EVENT_TRIGGER);
 	}
 
-	fflush(stdout);
-
-	return 0;
+	if (c == 'q') ev_break(EV_A_ EVBREAK_ALL);
 }
+
+#include "keyboard.inc"
 
 int main(void)
 {
 	mhsm_hsm_t switches[MONO_NROF_SWITCHES];
 	mono_state_t switch_states[MONO_NROF_SWITCHES];
+	ev_io stdin_watcher;
+	ev_timer print_timeout;
+	struct ev_loop *loop = EV_DEFAULT;
 	int i;
 
 	for (i = 0; i < MONO_NROF_SWITCHES; i++) {
-		mhsm_initialise(switches + i, switch_states + i, &mono_off);
-		if (mtmr_initialise_timers(switches + i, MONO_EVENT_TIMEOUT) != 0) {
-			MDBG_PRINT1("failed to initialise timers for hsm %d\n", i);
-			exit(EXIT_FAILURE);
-		}
+		mhsm_hsm_t *switch_hsm = switches + i;
+		mono_state_t *switch_state = switch_states + i;
 
-		switch_states[i].id = i + 1;
-		switch_states[i].counter = 0;
+		mhsm_initialise(switch_hsm, switch_state, &mono_off);
+		mtmr_ev_initalise_timers(switch_hsm, MONO_EVENT_TIMEOUT, loop);
 
-		mhsm_dispatch_event(switches + i, MHSM_EVENT_INITIAL);
+		switch_state->id = i + 1;
+		switch_state->counter = 0;
+
+		mhsm_dispatch_event(switch_hsm, MHSM_EVENT_INITIAL);
 	}
 
-	printf("press '1' to '%c' to enable lights\n", '1' + MONO_NROF_SWITCHES - 1);
+	ev_io_init(&stdin_watcher, keypress_cb, 0, EV_READ);
+	ev_io_start(loop, &stdin_watcher);
+	stdin_watcher.data = switches;
+
+	ev_timer_init(&print_timeout, print_cb, 0.1, 0.1);
+	ev_timer_start(loop, &print_timeout);
+	print_timeout.data = switches;
 
 	nonblock(1);
-	periodic(MONO_PERIOD, process, switches, switch_states);
-	nonblock(0);
-
+	printf("press '1' to '%c' to enable lights, 'q' to quit\n", '1' + MONO_NROF_SWITCHES - 1);
+	ev_run(loop, 0);
 	printf("\nquitting\n");
+	nonblock(0);
 
 	return 0;
 }
