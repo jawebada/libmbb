707833e030a359c544502e52b4aa37b0be494369
Jan Weil <jan.weil@web.de>
2015-02-17 16:27:04 +0100

    Factor out common timer macros

--- mbb/timer_common.h
+++ mbb/timer_common.h
@@ -18,22 +18,16 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef MBB_TIMER_H
-#define MBB_TIMER_H
+#ifndef MBB_TIMER_COMMON_H
+#define MBB_TIMER_COMMON_H
 
-#include "types.h"
 #include "hsm.h"
-#include "timer_common.h"
 
-typedef struct {
-	uint32_t period;
-	uint32_t value;
-	bool active;
-} mtmr_t;
+#define MTMR_NROF_TIMERS(LAST_TIMER_EVENT) (LAST_TIMER_EVENT - MHSM_EVENT_CUSTOM + 1)
 
-int mtmr_initialise_timers(mhsm_hsm_t *hsm, uint32_t last_timer_event);
-int mtmr_increment_timers(mhsm_hsm_t *hsm, uint32_t last_timer_event, uint32_t passed_msecs);
+#define MTMR_ONE_MSEC	(1)
+#define MTMR_ONE_SEC	(1000 * MTMR_ONE_MSEC)
+#define MTMR_ONE_MIN	(60 * MTMR_ONE_SEC)
+#define MTMR_ONE_HOUR	(60 * MTMR_ONE_MIN)
 
-int mtmr_start_timer(mhsm_hsm_t *hsm, uint32_t event_id, uint32_t period_msecs);
-
-#endif /* MBB_TIMER_H */
+#endif /* MBB_TIMER_COMMON_H */
